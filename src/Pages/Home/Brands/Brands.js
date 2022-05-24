a436cc41213ae403c91aea0a33d8c1e21d21e78b
showmittro <showmittrobiswas2000@gmail.com>
2022-05-24 22:20:23 +0600

    extra brand parts add

--- src/Pages/Home/Brands/Brands.js
+++ src/Pages/Home/Brands/Brands.js
@@ -1,24 +1,120 @@
 import React from 'react';
+import asus from '../../../images/asus.png'
+import dell from '../../../images/dell.png'
+import delux from '../../../images/delux.png'
+import econymy from '../../../images/econymy.webp'
+import hp from '../../../images/hp.png'
+import intel from '../../../images/intel.png'
+import pentium from '../../../images/pentium.jpg'
+import apple from '../../../images/apple.jpg'
+import lenovo from '../../../images/lenovo-.png'
 
 const Brands = () => {
     return (
-        <div className='py-10 text-center'>
-            <p className='text-4xl font-bold text-sky-400'>Our Brands</p>
-            <p className='text-5xl'>comming soons..........</p>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-10'>
+        <div className='py-12 text-center px-10'>
+            <p className='text-3xl font-bold text-sky-400'>Our Best Selling Products</p>
+            <p className='text-1xl'>Best Brands Parts for Laptop & Desktop</p>
+            <div className='grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3 gap-10 p-10'>
                 {/* brand-1  */}
-                <div class="card card-side bg-base-100 shadow-xl">
-                    <figure><img src="" alt="" /></figure>
+                <div class="card card-side bg-base-100 shadow-2xl">
+                    <figure><img style={{ height: '100px', width: '100px' }} src={asus} alt="" /></figure>
                     <div class="card-body">
-                        <h2 class="card-title">New movie is released!</h2>
-                        <p>Click the button to watch on Jetflix app.</p>
+                        <h2 class="card-title font-bold">Brand Name: Asus</h2>
+                        <p>ASUS is a leading company driven by innovation and commitment to quality.</p>
+                        <div class="card-actions justify-end">
+                            {/* <button class="btn btn-primary">Watch</button> */}
+                        </div>
+                    </div>
+                </div>
+                {/* brand-2  */}
+                <div class="card card-side bg-base-100 shadow-2xl">
+                    <figure><img style={{ height: '100px', width: '100px' }} src={dell} alt="" /></figure>
+                    <div class="card-body">
+                        <h2 class="card-title font-bold">Brand Name: Dell</h2>
+                        <p>Shop the latest Dell computers solutions. Laptops, desktops, gaming pc.</p>
+                        <div class="card-actions justify-end">
+                            {/* <button class="btn btn-primary">Watch</button> */}
+                        </div>
+                    </div>
+                </div>
+                {/* brand-3  */}
+                <div class="card card-side bg-base-100 shadow-2xl">
+                    <figure><img style={{ height: '100px', width: '100px' }} src={delux} alt="" /></figure>
+                    <div class="card-body">
+                        <h2 class="card-title font-bold">Brand Name: Delux</h2>
+                        <p>The designers of DELUX adhere to the principles of industrial design in line with human aesthetics.</p>
+                        <div class="card-actions justify-end">
+                            {/* <button class="btn btn-primary">Watch</button> */}
+                        </div>
+                    </div>
+                </div>
+                {/* brand-4  */}
+                <div class="card card-side bg-base-100 shadow-2xl">
+                    <figure><img style={{ height: '100px', width: '100px' }} src={econymy} alt="" /></figure>
+                    <div class="card-body">
+                        <h2 class="card-title font-bold">Brand Name: Econymy</h2>
+                        <p>An economy encompasses all activity related to production of goods and services in an area.</p>
+                        <div class="card-actions justify-end">
+                            {/* <button class="btn btn-primary">Watch</button> */}
+                        </div>
+                    </div>
+                </div>
+                {/* brand-5  */}
+                <div class="card card-side bg-base-100 shadow-2xl">
+                    <figure><img style={{ height: '100px', width: '100px' }} src={hp} alt="" /></figure>
+                    <div class="card-body">
+                        <h2 class="card-title font-bold">Brand Name: HP</h2>
+                        <p>Learn about HP laptops, pc desktops, printers, accessories at the Official HP® Website</p>
+                        <div class="card-actions justify-end">
+                            {/* <button class="btn btn-primary">Watch</button> */}
+                        </div>
+                    </div>
+                </div>
+                {/* brand-6  */}
+                <div class="card card-side bg-base-100 shadow-2xl">
+                    <figure><img style={{ height: '100px', width: '100px' }} src={intel} alt="" /></figure>
+                    <div class="card-body">
+                        <h2 class="card-title font-bold">Brand Name: Intel</h2>
+                        <p>Intel's innovation in cloud computing, data center, Internet of Things, and PC solutions.</p>
+                        <div class="card-actions justify-end">
+                            {/* <button class="btn btn-primary">Watch</button> */}
+                        </div>
+                    </div>
+                </div>
+                {/* brand-7 */}
+                <div class="card card-side bg-base-100 shadow-2xl">
+                    <figure><img style={{ height: '100px', width: '100px' }} src={pentium} alt="" /></figure>
+                    <div class="card-body">
+                        <h2 class="card-title font-bold">Brand Name: Pentium</h2>
+                        <p>Pentium is a brand used for a series of x86 architecture-compatible microprocessors.</p>
+                        <div class="card-actions justify-end">
+                            {/* <button class="btn btn-primary">Watch</button> */}
+                        </div>
+                    </div>
+                </div>
+                {/* brand-8  */}
+                <div class="card card-side bg-base-100 shadow-2xl">
+                    <figure><img style={{ height: '100px', width: '100px' }} src={apple} alt="" /></figure>
+                    <div class="card-body">
+                        <h2 class="card-title font-bold">Brand Name: Apple</h2>
+                        <p>Discover the innovative world of Apple and shop everything iPhone, iPad, Apple Watch, Mac, and Apple TV, plus explore.</p>
+                        <div class="card-actions justify-end">
+                            {/* <button class="btn btn-primary">Watch</button> */}
+                        </div>
+                    </div>
+                </div>
+                {/* brand-9  */}
+                <div class="card card-side bg-base-100 shadow-2xl">
+                    <figure><img style={{ height: '100px', width: '100px' }} src={lenovo} alt="" /></figure>
+                    <div class="card-body">
+                        <h2 class="card-title font-bold">Brand Name: Lenovo</h2>
+                        <p>Lenovo Group Limited, often shortened to Lenovo is a Chinese multinational technology company manufacturing.</p>
                         <div class="card-actions justify-end">
                             {/* <button class="btn btn-primary">Watch</button> */}
                         </div>
                     </div>
                 </div>
             </div>
-
         </div>
     );
 };
--- src/Pages/Home/Home/BusinessSumary/BusinessSumary.js
+++ src/Pages/Home/Home/BusinessSumary/BusinessSumary.js
@@ -1,33 +1,45 @@
 import React from 'react';
 import countriesIcons from '../../../../images/Flag.png'
-import revenueIcons from '../../../../images/Revenuejpg.jpg'
+import revenueIcons from '../../../../images/Revenue.jpg'
 import feedbackIcons from '../../../../images/feedback.png'
 
 const BusinessSumary = () => {
     return (
         <div className='py-10 px-12'>
             <h1 class="text-4xl font-bold text-center">This is Business Summary</h1>
-            <div class="stats shadow-lg grid grid-cols-2 md:gird-cols-2 lg:grid-cols-3 gap-5 py-10 ">
 
-                <div class="stat place-items-center">
-                    <div class="stat-title"><img className='py-5' style={{height:'130px',width:'130px'}} src={countriesIcons} alt="" /></div>
-                    <div class="stat-value"> <p className='text-5xl text-secondary'>65</p></div>
-                    <div class="stat-desc"><p className='text-2xl pt-1'>Countries</p></div>
+            
+
+            <div className='grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3 gap-10 p-10'>
+                <div class="card w-50 bg-base-100 shadow-2xl">
+                    <figure class="px-10 pt-10">
+                        <img style={{ height: '100px', width: '100px' }} src={countriesIcons} alt="Shoes" class="rounded-xl" />
+                    </figure>
+                    <div class="card-body items-center text-center">
+                        <p className='text-5xl text-secondary font-bold'>65</p>
+                        <div class="stat-desc"><p className='text-2xl pt-1'>Countries</p></div>
+                    </div>
                 </div>
-
-                <div class="stat place-items-center">
-                <div class="stat-title"><img className='py-5' style={{height:'130px',width:'130px',}} src={revenueIcons} alt="" /></div>
-                <div class="stat-value"> <p className='text-5xl text-secondary'>420M+</p></div>
-                    <div class="stat-desc"><p className='text-2xl pt-1'>Revenue</p></div>
+                <div class="card w-50 bg-base-100 shadow-2xl">
+                    <figure class="px-10 pt-10">
+                        <img style={{ height: '100px', width: '100px' }} src={revenueIcons} alt="Shoes" class="rounded-xl" />
+                    </figure>
+                    <div class="card-body items-center text-center">
+                        <p className='text-5xl text-secondary font-bold'>420M+</p>
+                        <div class="stat-desc"><p className='text-2xl pt-1'>Revenue</p></div>
+                    </div>
                 </div>
-
-                <div class="stat place-items-center">
-                <div class="stat-title"><img className='py-5' style={{height:'100px',width:'100px'}} src={feedbackIcons} alt="" /></div>
-                <div class="stat-value"> <p className='text-5xl text-secondary'>770+</p></div>
-                    <div class="stat-desc"><p className='text-2xl pt-1'>Feedback</p></div>
+                <div class="card w-50 bg-base-100 shadow-2xl">
+                    <figure class="px-10 pt-10">
+                        <img style={{ height: '100px', width: '100px' }} src={feedbackIcons} alt="Shoes" class="rounded-xl" />
+                    </figure>
+                    <div class="card-body items-center text-center">
+                        <p className='text-5xl text-secondary font-bold'>770+</p>
+                        <div class="stat-desc"><p className='text-2xl pt-1'>Feedback</p></div>
+                    </div>
                 </div>
-
             </div>
+
         </div>
     );
 };

dc931e746c18998813694a3700484d24381cc4aa
showmittro <showmittrobiswas2000@gmail.com>
2022-05-26 02:45:23 +0600

    add a reviews section design

--- src/Pages/Dashboard/Review/Review.js
+++ src/Pages/Dashboard/Review/Review.js
@@ -36,11 +36,11 @@ const Review = () => {
             <div className="add-review">
             <h2 className="text-center">Customer Review</h2>
             <form onSubmit={handleSubmit(onSubmit)}>
-            <input defaultValue={user.displayName} {...register("name")} />
-                <textarea {...register("description")} placeholder="Description" />
-                <input type="number" {...register("rating")} placeholder="rating" />
-                <input {...register("img")} placeholder="image url" />
-                <input type="submit" />
+            <input  style={{border:'1px solid green'}} className='w-full' defaultValue={user.displayName} {...register("name")}  />
+                <textarea  style={{border:'1px solid green'}} className='w-full' {...register("description")} placeholder="Description" />
+                <input  style={{border:'1px solid green'}} className='w-full' type="number" {...register("rating")} placeholder="rating" />
+                <input  style={{border:'1px solid green'}} className='w-full' {...register("img")} placeholder="image url" />
+                <input  style={{border:'1px solid green'}} className='w-full' type="submit" />
             </form>
         </div>
         </div>
